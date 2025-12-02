5122965ab940ee42c1024eefff3afd4234de46aa
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-01 21:04:16 -0400

    add `ffmpeg-mini`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,7 +10,7 @@ pacman -Syu --noconfirm pipewire-audio pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
 make-aur-package --chaotic-aur qimgv-git
