bf9abee97e9cb934307d4b392ede1dec7b17ebe2
Samueru-sama <xdglawyer@outlook.com>
2026-08-07 03:10:06 -0400

    update to latest template

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,47 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm pipewire-audio pipewire-jack
+pacman -Syu --noconfirm \
+	cmake             \
+	exiv2             \
+	mpv               \
+	ninja             \
+	opencv            \
+	pipewire-audio    \
+	pipewire-jack     \
+	qt6-5compat       \
+	qt6-imageformats  \
+	qt6-multimedia    \
+	qt6-svg           \
+	qt6-tools
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
-make-aur-package --chaotic-aur qimgv-git
+#make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
+
+# build qimgv manually from the latest stable tag
+echo "Building qimgv..."
+echo "---------------------------------------------------------------"
+git clone https://github.com/easymodo/qimgv.git ./qimgv
+cd ./qimgv
+
+git fetch --tags origin
+TAG=$(git tag --sort=-v:refname | grep -vi 'rc\|alpha\|beta' | head -1)
+git checkout "$TAG"
+
+mkdir -p ./build
+cd ./build
+cmake ../ \
+	-G Ninja                    \
+	-DCMAKE_BUILD_TYPE=Release  \
+	-DCMAKE_INSTALL_PREFIX=/usr \
+	-DCMAKE_INSTALL_LIBDIR=lib
+cmake --build ./ -j"$(nproc)"
+cmake --install ./
+
+echo "$TAG" > ~/version
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,24 +3,26 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q qimgv-git | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/qimgv.png
 export DESKTOP=/usr/share/applications/qimgv.desktop
-export ANYLINUX_LIB=1
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun \
-	/usr/bin/qimgv   \
-	/usr/lib/qimgv/* \
+	/usr/bin/qimgv \
+	/usr/lib/qimgv \
 	/usr/share/qimgv
 
 # Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --test ./dist/*.AppImage
